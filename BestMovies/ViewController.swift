2e7b9c3ed3c6c8f686a8184f9f1e1024cce7f7f1
maged <maged12345>
2025-05-15 17:20:40 +0300

    Initial Commit

---
+++ BestMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BestMovies
+//
+//  Created by maged on 15/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
